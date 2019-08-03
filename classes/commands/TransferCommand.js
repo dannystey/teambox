d5acc6fb59c9c6e7ed7d08800f54b030222887e4
Danny Stey <danny.stey@virtual-identity.com>
2019-08-04 00:52:59 +0200

    starting with transfer command

--- classes/commands/TransferCommand.js
+++ classes/commands/TransferCommand.js
@@ -11,26 +11,6 @@ class IgnoreCommand {
         
     }
 
-    save(dates) {
-        let datesStr = dates.join(',');
-        let content = fs.readFileSync(this.rootPath + '/.env', 'utf-8');
-        content = content.replace(/(EXCLUDEDAYS=)(.*)/mg, `$1${datesStr}`);
-        if(fs.existsSync(this.rootPath + '/.env')) {
-            fs.writeFile(this.rootPath + '/.env', content, { encoding: 'utf8' }, (err) => {
-                if(err) {
-                    console.error(err);
-                    process.exit(1);
-                }
-                console.log('done!');
-                process.exit();
-            });
-        }
-        else {
-            console.log('please register the teambox first!');
-        }
-
-    }
-
 }
 
 
